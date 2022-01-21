2c101a36330b79b7c4d89de63c5b2de07b676b93
Lisa Murray <lisamichellemurray@gmail.com>
2022-01-21 05:27:15 +0000

    react form gets tests per visit per study

--- static/js/results_form.jsx
+++ static/js/results_form.jsx
@@ -1,23 +1,20 @@
 
 function ResultsForm() {
     // create form input of study names in dropdown 
-
     const [studies, setStudies] = React.useState([]);
-
     React.useEffect(() =>  {
         fetch('/studies.json')
         .then(response => response.json())
         .then(result => setStudies(result))
         }, []);
     
+    // create visit input based on selected study
     const [studySelected, setStudySelected] = React.useState(null);
     const [visits, setVisits] = React.useState([]);
 
     React.useEffect(() => { 
-        if (studySelected === null) {
-            return
-        }
-        console.log(`Selected has changed to ${studySelected}`)
+        if (studySelected === null) { return }
+        console.log(`STUDY Selected has changed to ${studySelected}`)
         fetch(`/visits-results.json/${studySelected}`)
         .then(response => response.json())
         .then(result => setVisits(result))
@@ -30,23 +27,61 @@ function ResultsForm() {
         setStudySelected(id);
     }
 
+    // create test input based on selected visit
+    const [visitSelected, setVisitSelected] = React.useState(null);
+    const [tests, setTests] = React.useState([]);
+
+    React.useEffect(() => { 
+        if (visitSelected === null) { return }
+        const tests = []
+        console.log(`VISIT Selected has changed to ${visitSelected}`)
+        for (const visititem of visits) {
+            if (visititem.visit === visitSelected) {
+                tests.push(visititem)
+            }
+        }
+        console.log("visits", visits)
+        console.log(visitSelected)
+        console.log("tests", tests)
+        setTests(tests)
+    }, [visitSelected]);
+
+    const handleVisitSelected = evt => {
+        const id = evt.target.value;
+        console.log("handle", id);
+        console.log(evt)
+        setVisitSelected(id);
+    }
+
+    // render form html
     return (
         <div>
-            <form>     
-                <label>select the study you will input results for</label>               
+            <label>select the study you will input results for</label>               
                 <select id="study_id" name="study" value="I RULE" onChange={handleStudySelected}>
                     <option selected disabled>Choose here</option>
                     {studies.map((data) => (
                     <option key={data.study_id} value={data.study_id}> {data.study_name}</option>
                      ))}
                 </select>
-                <select id="visit" name="visit">
-                    <option selected disabled>Choose here</option>
+            <form>  
+                <label>participant ID</label>
+                <input type="number" name="participant-id"/>   
+                <lable>study visit</lable>
+                <select id="visit" name="visit" onChange={handleVisitSelected}>
+                    <option selected disabled >Choose here</option>
                     {visits.map((data) => (
-                    <option key="{data.visit}" value="{data.visit}"> {data.visit}</option>
+                    <option key={data.visit} value={data.visit}> {data.visit}</option>
                      ))}
                 </select>
-            <div>This will be results input</div>
+                <label>test</label>
+                <select id="test" name="test">
+                    <option selected disabled>Choose here</option>
+                    {tests.map((data) => (
+                    <option key={data.result_plan_id} value={data.result_plan_id}> {data.test_name}</option>
+                     ))}
+                </select>
+                <label>test result</label>
+                <input type="text" name="result-value"/>
             </form>
         </div>        
     )
