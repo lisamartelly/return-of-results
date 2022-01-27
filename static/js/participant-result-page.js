f0ae853053b70132b04b4de5a73c01cd2d1cb82b
Lisa Murray <lisamichellemurray@gmail.com>
2022-01-27 21:30:04 +0000

    updated results displays for participants and investigators

---
+++ static/js/participant-result-page.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const tables = document.querySelectorAll(".result-table")
+
+for (const table of tables){
+    const length = table.rows.length;
+    console.log("table length: ", table, length)
+    if (length === 1) {
+        table.innerHTML = '<li>No results will be returned during this time period</li>';
+    }
+}
