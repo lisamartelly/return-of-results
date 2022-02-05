105cc7bf4a0b6192436de50900fe347b80649e27
Lisa Murray <lisamichellemurray@gmail.com>
2022-02-05 23:09:22 +0000

    added query string check on page load

--- static/js/enrollment.js
+++ static/js/enrollment.js
@@ -1,5 +1,12 @@
 'use strict';
 
+
+// autofill study id if included in query string
+const urlParams = new URLSearchParams(window.location.search);
+document.querySelectorAll('#study_id').forEach(item => {
+    item.value = urlParams.get('studyId')
+})
+
 function checkParticipantId(evt) {
     evt.preventDefault();
 
