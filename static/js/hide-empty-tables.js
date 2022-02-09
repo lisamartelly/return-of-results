c52a5a092a0cbbf4f1d1c79a5aa899e7492449ba
Lisa Murray <lisamichellemurray@gmail.com>
2022-02-09 05:26:34 +0000

    updated participant results display

--- static/js/hide-empty-tables.js
+++ static/js/hide-empty-tables.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // hides empty tables on participants details page using the number of rows in the table
-const tables = document.querySelectorAll(".result-table")
+const tables = document.querySelectorAll(".pt-facing-details-table")
 
 for (const table of tables){
     const length = table.rows.length;
